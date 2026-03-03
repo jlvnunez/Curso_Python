3224496e61658f0b28c4f33e922ebe6209a0b441
jlvnunez <jlvnunez001@gmail.com>
2026-03-02 23:34:11 -0300

    python mundo3 update

--- Mundo_03/curso_em_video/ex075AnaliseDadosEmTupla.py
+++ Mundo_03/curso_em_video/ex075AnaliseDadosEmTupla.py
@@ -6,15 +6,19 @@ C) Quais foram os números pares.'''
 print()
 print(f'{"Analise de dados em Tupla":=^50}')
 print('='*50)
+
 num = (int(input('digite o primeiro valor '))),(int(input('digite o segundo valor '))),(int(input('digite o terceiro valor '))),(int(input('digite o quarto valor ')))
+
 print('='*50)
 print(f'Voce digitou: {num}')
 print(f'O valor 9 aparece {num.count(9)} vezes')
+
 if 3 in num:
     print(f'O valor 3 apareceu na {num.index(3)+1}ª posicao')
 else:
     print('\033[1;31mO valor 3 nao foi digitado em nenhuma posição!\033[m')
-print(f'Os valores pares digitados foram: ',end='  ')
+
+print(f'Os valores pares digitados foram:',end=' ')
 for p in num:
     if p %2==0:
         print(p,end=' ')
