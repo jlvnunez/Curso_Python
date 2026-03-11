6c52c10834046f44eef0770e760701375947b0e2
jlvnunez <jlvnunez001@gmail.com>
2026-03-11 16:35:49 -0300

    Create ex093.py

---
+++ Mundo_03/curso_em_video/ex093.py
@@ -0,0 +1,22 @@
+#Exercício Python 093: Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+
+print(f'{"Cadastro de Jogadores":=^50}')
+
+jogador={}
+partidas=[]
+jogador['nome']=str(input('Nome Jogador '))
+tot= int(input(f'Total de partidas de {jogador['nome']} '))
+for c in range (0,tot):
+    partidas.append(int(input(f' Quantos gol na partida {c+1}? ')))
+jogador['gols'] = partidas[:]
+jogador['total']= sum(partidas)
+print('='*50)
+print(jogador)
+print('='*50)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor de {v}')
+print('='*50)
+print(f' O Jogador {jogador['nome']} jogou {len(jogador['gols'])} partidas')
+for i ,v in enumerate(jogador['gols']):
+    print(f'  ==> Na partida {i+1} fez {v} gols')
+print(f'Foi um total de {jogador['total']} gols')
