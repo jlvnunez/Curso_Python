eb273a41b70168e7b05d00097202858b2940d7e5
jlvnunez <jlvnunez001@gmail.com>
2026-03-16 16:25:07 -0300

    python mundo 3 update

--- Mundo_03/curso_em_video/ex094.py
+++ Mundo_03/curso_em_video/ex094.py
@@ -1,6 +1,17 @@
 #Exercício Python 094: Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre: A) Quantas pessoas foram cadastradas B) A média de idade C) Uma lista com as mulheres D) Uma lista de pessoas com idade acima da média
 
-print(f'{"Cadastro de Jogadores":=^50}')
+print(f'{"Unindo Dicionarios e Listas":=^50}')
 
-dados={}
-lista=[]
+pessoa={}
+
+pessoa["nome"]=str(input("Nome "))
+while True:
+    pessoa["sexo"] = str(input("Sexo [M/F] ")).upper[0]
+    if pessoa["sexo"]in "MF":
+        break
+    else:
+        print("digite apenas M ou F")
+
+
+#pessoa["idade"]=int(input("Idade "))
+
