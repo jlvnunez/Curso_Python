ddf4cc08602969422e00d04159b9169b82e22c9e
jlvnunez <jlvnunez001@gmail.com>
2026-03-17 21:56:32 -0300

    python mundo 3 update

---
+++ Mundo_03/curso_em_video/ex100.py
@@ -0,0 +1,28 @@
+#Exercício Python 100: Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar(). A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
+from random import randint
+from time import sleep
+print('-='*30)
+print(f'{"     Funções para Sortear e  somar     "}')
+print('-='*30)
+def sorteia(lista):
+    print('Sorteando 5 valores da lista ',end='')
+    for cont in range(0,5):
+        n =(randint(1,10))
+        lista.append(n)
+        print(f' {n} ',end='',flush=True)
+        sleep(0.3)
+    print('PRONTO!')
+
+def somaPar(lista):
+    soma=0
+    for valor in lista:#(varrendo a lista)
+        if valor %2==0:
+            soma+= valor
+    print(f'Somando os valores Pares de {lista}, temos {soma}')         
+    print('-='*30)
+numeros=list()
+sorteia(numeros)
+somaPar(numeros)
+
+
+
